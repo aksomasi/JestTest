22447a3bdcf30906e487576008ddc5d7169d9846
asomasi <anil.somasi@infor.com>
2019-12-04 19:35:26 +0530

    added header content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { PlayerLimitControlNames, stateEnum } from './Survey.modal';
+import { TranslateService } from '@ngx-translate/core';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Jest-Practise';
+  readonly playerLimitControlNames: typeof PlayerLimitControlNames = PlayerLimitControlNames ;
+  constructor(private translateService: TranslateService){
+    this.translateService.addLangs(['ap_en']);
+    this.translateService.setDefaultLang('ap_en');
+  }
+
+  next(val) {
+    alert(val);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,33 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+
+
+/**
+ * Get translation file location to initialize TranslateHttpLoader
+ */
+export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
+  return new TranslateHttpLoader(http, './assets/', '.json');
+}
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    }),
     AppRoutingModule
   ],
   providers: [],
